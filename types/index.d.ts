e9dca3bde3691f7a92928efd1c1d104d005b706c
Gideon Appau <gideonad.codez@gmail.com>
2022-03-29 18:11:20 +0000

    Initial Commit

--- types/index.d.ts
+++ types/index.d.ts
@@ -6,3 +6,7 @@ export declare function bounce({ animate, duration, delay, setStyle }: Animate):
 export declare function flip({ animate, duration, delay, setStyle }: Animate): void
 export declare function swing({ animate, duration, delay, setStyle }: Animate): void
 export declare function bubble({ animate, duration, delay, setStyle }: Animate): void
+export declare function slideFromLeft({ animate, duration, delay, setStyle }: Animate): void
+export declare function slideFromTop({ animate, duration, delay, setStyle }: Animate): void
+export declare function slideFromBottom({ animate, duration, delay, setStyle }: Animate): void
+export declare function slideFromRight({ animate, duration, delay, setStyle }: Animate): void
